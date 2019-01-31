847d928dcaf8c1aa388f1ee4140b41e0d1a1c366
Doortje_Steketee <47185237+DoortjeSteketee@users.noreply.github.com>
2019-01-31 15:30:43 +0100

    Update 5. feature_functions.py

--- Scripts/5. feature_functions.py
+++ Scripts/5. feature_functions.py
@@ -1,4 +1,5 @@
 ##Functions to find classification features
+##Below function is distance number in classifier. Order is different based on saliency of features
 
 ##Punctuation and numbers
 
@@ -6,31 +7,37 @@ def num_comma(text_tweet):
     """Counting the number of comma's in a tweet"""
     num_comma = text_tweet.count(',')
     return(num_comma)
+#num_comma = 15
 
 def num_period(text_tweet):
     """Counting the number of periods in a tweet"""
     num_period = text_tweet.count('.')
     return(num_period)
+#num_period = 16
 
 def num_question_mark(text_tweet):
     """Counting the number of question marks in a tweet"""
     num_question_mark = text_tweet.count('?')
     return(num_question_mark)
+#num_question_mark = 17
 
 def num_lowercase(text_tweet):
     """Counting the number of lowercase letters in a tweet"""
     num_lowercase = sum(1 for c in text_tweet if c.islower())
     return(num_lowercase)
+#num_lowercase = 10
 
 def num_uppercase(text_tweet):
     """Counting the number of uppercase letters in a tweet"""
     num_uppercase = sum(1 for c in text_tweet if c.isupper())
     return(num_uppercase)
+#num_uppercase = 11
 
 def num_numeric(text_tweet):
     """Counting the number of numeric characters in a tweet"""
     num_numeric = sum(1 for c in text_tweet if c.isnumeric())
     return(num_numeric)
+#num_numeric = 19
 
 def count_symbols(text_tweet):
     """Counting the amount of symbols in a tweet"""
@@ -39,11 +46,13 @@ def count_symbols(text_tweet):
     special_chars = string.punctuation
     num_symbols = sum(v for k, v in ct.Counter(text_tweet).items() if k in special_chars)
     return(num_symbols)
+#count_symbols = 20
 
 def count_exl_marks(text_tweet):
     """Counting exclamation marks in a tweet"""
     count_exl_mark = text_tweet.count("!")
     return(count_exl_mark)
+#count_exl_marks = 18
 
 
 ##Extra-linguistic features
@@ -52,6 +61,7 @@ def count_reply_tweet(text_tweet):
     """Counting the amount of times an author replies to someone using '@'"""
     count_reply = text_tweet.count('@')
     return(count_reply)
+#count_reply_tweet = 1
 
 def contains_links(text_tweet):
     """Determining if a tweet contains a link"""
@@ -60,6 +70,7 @@ def contains_links(text_tweet):
     if "https:" not in text_tweet:
         contains_link = 0
     return(contains_link)
+#contains_link = 2 
 
 def contains_smiley(text_tweet):
     """Determining if a tweet contains a happy smiley"""
@@ -68,6 +79,7 @@ def contains_smiley(text_tweet):
     if " :) " not in text_tweet:
         contains_smiley = 0
     return(contains_smiley)
+#contains_smiley = 3
 
 ##Linguistic features
 
@@ -80,6 +92,7 @@ def count_number_verbs(text_tweet):
         pos_tags.append(pos_tag)
     count_verbs = pos_tags.count("VERB")
     return(count_verbs)
+#count_number_verbs = 6
 
 def count_number_nouns(text_tweet):
     """Counting the number of nouns in a tweet"""
@@ -90,17 +103,20 @@ def count_number_nouns(text_tweet):
         pos_tags.append(pos_tag)
     count_nouns = pos_tags.count("NOUN")
     return(count_nouns)
+#count_number_nouns = 5
 
 def count_tokens(text_tweet):
     """Counting the amount of tokens (words) in a tweet"""
     doc = nlp(text_tweet)
     num_tokens = len(doc)
     return(num_tokens)
+#count_tokens = 7
 
 def num_like(text_tweet):
     """Counting the amount of times the author uses the word 'like'"""
     number_likes = text_tweet.count(" like ") + text_tweet.count(" Like ") + text_tweet.count("Like ")
     return(number_likes)
+#num_like = 8
 
 def num_first_person(text_tweet):
     """Counting the number of first person singular expressions"""
@@ -112,6 +128,7 @@ def num_first_person(text_tweet):
     num_Iam = text_tweet.count(" I am ") + text_tweet.count("I am ")
     num_first_person = num_I + num_my + num_Im + num_Ihave + num_me + num_Iam
     return(num_first_person)
+#num_first_person = 9
 
 ##Sentiment features
 
@@ -122,6 +139,7 @@ def positive_scores(text_tweet):
     polarity_score = doc._.polarity_scores
     polarity_scores(doc)
     return(polarity_score["pos"])
+#positive_scores = 12
 
 
 def negative_scores(text_tweet):
@@ -131,6 +149,7 @@ def negative_scores(text_tweet):
     polarity_score = doc._.polarity_scores
     polarity_scores(doc)
     return(polarity_score["neg"])
+#negative_scores = 13
 
 def neutral_scores(text_tweet):
     """Measuring the neutral sentiment of a text in a tweet"""
@@ -139,3 +158,4 @@ def neutral_scores(text_tweet):
     polarity_score = doc._.polarity_scores
     polarity_scores(doc)
     return(polarity_score["neu"])
+#neutral_scores = 14
